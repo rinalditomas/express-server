36a3a370009139f36267fbf04ccc704aa4fe288c
rinalditomas <rinalditomas@gmail.com>
2023-06-04 22:42:11 +0200

    a

--- src/generate_pdf.ts
+++ src/generate_pdf.ts
@@ -1,6 +1,5 @@
-const puppeteer = require('puppeteer');
-const path = require('path');
-
+import puppeteer from 'puppeteer';
+import path = require('path');
 declare const document: any; // Add this line to resolve the error
 
 const months = [
