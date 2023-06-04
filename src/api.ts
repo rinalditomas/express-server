f6c41c1e587102b8dc58560b074b1bff4db18cfc
rinalditomas <rinalditomas@gmail.com>
2023-06-04 18:06:12 +0200

    Changes

--- src/api.ts
+++ src/api.ts
@@ -20,5 +20,21 @@ api.get('/hello', (req, res) => {
   res.status(200).send({ message: 'hello world' });
 });
 
+
+// Accepts POST requests at /webhook endpoint
+app.post("/webhook", (req, res) => {
+  res.status(200).send('webhook post')
+});
+
+// Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests 
+app.get("/webhook", (req, res) => {
+
+  
+  res.status(200).send('webhook get');
+  
+});
+
+
 // Version the api
 app.use('/api/v1', api);
