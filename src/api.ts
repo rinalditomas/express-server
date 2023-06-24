659b9779b047a69c4d02d72e1d1462f570deef09
rinalditomas <rinalditomas@gmail.com>
2023-06-24 10:52:26 +0200

    CONSOLE.LOGIN THE MESSSAGE

--- src/api.ts
+++ src/api.ts
@@ -90,10 +90,13 @@ app.get('/media', async (req, res) => {
 });
 
 app.post('/webhook', async (req, res) => {
-
+  console.log(
+    'HERE IS THE CONSOLE.LOG IN WEBHOOK POST',
+    JSON.stringify(req.body, null, 2)
+  );
   const message = req.body.entry[0].changes[0].value;
 
-  console.log('THIS IS THE MESSAGE', message);
+  console.log("THIS IS THE MESSAGE:'", message);
   // // info on WhatsApp text message payload: https://developers.facebook.com/docs/w
   // if (message.type === 'text') {
   //   const hoursWorked = parseFloat(message.text.body);
